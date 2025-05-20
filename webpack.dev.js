b9642ed6eab0a3a24a002f2005735fe0bc044706
rustleium <189756814+rustleium@users.noreply.github.com>
2025-05-20 12:57:19 +0530

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,14 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    watchFiles: ['./src/template.html'],
+    hot: true,
+    open: true,
+  }
+})
